73405989ebbcc14357224c678b9e49d04cedee25
VyachTur <tamplier29@mail.ru>
2023-02-13 17:06:15 +1000

    Refactoring. Add Input System and Interfaces. Add circle view for second attack in Editor.

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
@@ -12,28 +13,53 @@ namespace Player
         [SerializeField] private Animator _playerAnimator;
 
         private CharacterController _characterController;
-        private float _verticalInput;
-        private float _horizontalInput;
-        private Vector3 _dir;
+        // private float _verticalInput;
+        // private float _horizontalInput;
+        private IInputMove _inputMove;
+        private Vector3 _direction;
+
+        private void Awake()
+        {
+            _inputMove = FindObjectOfType<PlayerInput>();
+            _inputMove.OnPlayerMoveEvent += OnSetInputDirection;
+        }
+
+        private void OnSetInputDirection(Vector2 direction)
+        {
+            _direction = direction;
+        }
 
         private void Start()
         {
             TryGetComponent(out _characterController);
         }
 
-        private void Update()
-        {
-            _verticalInput = Input.GetAxis("Vertical");
-            _horizontalInput = Input.GetAxis("Horizontal");
-        }
+        // private void Update()
+        // {
+        //     // _verticalInput = Input.GetAxis("Vertical");
+        //     // _horizontalInput = Input.GetAxis("Horizontal");
+
+
+        // }
+
+        // public void OnMovement(InputAction.CallbackContext value)
+        // {
+        //     Vector2 inputMovement = value.ReadValue<Vector2>();
+            
+        //     _direction = new Vector3(inputMovement.x * _moveSpeed, _gravity, inputMovement.y * _moveSpeed);
+
+        //     print("OnMovement");
+        // }
 
         private void FixedUpdate()
         {
-            _dir = new Vector3(_horizontalInput * _moveSpeed, _gravity, _verticalInput * _moveSpeed) * Time.fixedDeltaTime;
+            _direction = new Vector3(_direction.x * _moveSpeed, _gravity, _direction.y * _moveSpeed) * Time.fixedDeltaTime;
 
-            _characterController.Move(_dir); // player move fith gravity
+            // _direction = _direction * Time.fixedDeltaTime;
 
-            // Debug.DrawRay(transform.position, new Vector3(_dir.x, 0f, _dir.z) * 10f, Color.red);
+            _characterController.Move(_direction); // player move fith gravity
+
+            // Debug.DrawRay(transform.position, new Vector3(_direction.x, 0f, _direction.z) * 10f, Color.red);
 
         }
     }
